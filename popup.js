f80e0a11e252e31ab1373820692444a0a26dc263
Zanclovic <38193673+Zanclovic@users.noreply.github.com>
2020-09-30 11:01:14 -0500

    Added style.css & fixed popup.js

--- popup.js
+++ popup.js
@@ -16,7 +16,6 @@ changeColor.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";';
-        document.getElementsByClassName("QRiHXd").style.color = "' + color + '";
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
