cf2f65073d5e787081be6e7e1413e41581af289b
Zanclovic <38193673+Zanclovic@users.noreply.github.com>
2020-10-23 12:08:35 -0500

    big version 2 inject not working

--- background.js
+++ background.js
@@ -3,9 +3,13 @@
 // found in the LICENSE file.
 
 'use strict';
+var buttoninsert = '<span jsslot="" jsname="j7LFlb" tabindex="-1" class="z80M1 FeRvI NZltnf" jsaction="click:o6ZaF(preventDefault=true); mousedown:lAhnzb; mouseup:Osgxgf; mouseenter:SKyDAe; mouseleave:xq3APb;touchstart:jJiBRc; touchmove:kZeBdd; touchend:VfAz8(preventMouseEvents=true)" aria-label="Slides" role="menuitem"><div class="aBBjbd MbhUzd" jsname="ksKsZd"></div><div class="PCdOIb Ce1Y1c" aria-hidden="true"><div class="IVUIAc"></div></div><div class="uyYuVb oJeWuf" jsaction="JIbuQc:v9F4Yd" data-type="3"><div class="jO7h3c">CUSTOM BUTTON</div></div></span>';
+document.getElementsByClassName ("z80M1 FeRvI NZltnf")[0] = buttoninsert;
 
+var hideinsert = '<div class="jO7h3c">Hide</div>';
+document.getElementsByClassName("uyYuVb oJeWuf")[0] = hideinsert;
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#2C2F33'}, function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
--- options.js
+++ options.js
@@ -5,7 +5,7 @@
 'use strict';
 
 let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#2C2F33', '#e8453c', '#f9bb2d', '#4688f1'];
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
